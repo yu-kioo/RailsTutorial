bfec946c1accb895dfe3bbed28199ac8d97652e0
yuki-tashiro <>
2019-08-14 15:33:53 +0900

    add：minitest-reporter , Guard追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+# add：テスト結果の表示をわかりやすく
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+	# Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+	fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+	# Add more helper methods to be used by all tests here...
 end
