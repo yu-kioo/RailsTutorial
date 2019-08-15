a1ea8ecc2cbe6079b62bb35b57a5d9e7088f6fb9
yuki-tashiro <>
2019-08-15 12:48:48 +0900

    fix：タイトル表示のリファ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # ページタイトルが定義されている時はタイトルにbase_titleを付加して返し、
+  # タイトルがない場合はbase_titleのみ返す
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     assert_response :success
     # htmlのタグ内に指定文字列が存在するか
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
 	end
 
 	test 'should get help' do
